dbacabd161ed1d828a23d1c83e479579a19ea38e
Addiction023 <atulpatel09876@gmail.com>
2026-07-27 20:45:24 +0530

    express

---
+++ ExpressApp/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Welcome to About Page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Welcome to Contact Page");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
