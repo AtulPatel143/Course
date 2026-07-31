ff9cfbbd44ffe5277c8082710466b85ee140faba
Addiction023 <atulpatel09876@gmail.com>
2026-07-31 21:55:02 +0530

    ejs complete 99%

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
+
 
 const userRouter = require("./routes/user");
 const productRouter = require("./routes/product");
@@ -7,6 +9,19 @@ const fruitRouter = require("./routes/fruits");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.use((req, res, next) => {
+  if (req.query.admin !== "yes") {
+    return res.status(403).send("Access denied");
+  }
+  next();
+});
 
 app.use("/users", userRouter);
 app.use("/products", productRouter);
